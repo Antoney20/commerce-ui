79bf67dfde1615491cefd6e1cd07a0a3edd641ca
ANTONY klerk <antomutwiri2001@gmail.com>
2023-12-12 17:06:14 +0300

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Nav  from "./components/Nav.jsx";
 import Banner from './components/Banner.jsx'
+
 import Home from './components/Home.jsx'
+import Products from './components/Products'
 import Blog from './components/Blog.jsx'
+
 
 
 
@@ -18,6 +21,7 @@ export default function App() {
     <Nav/>
     <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
         <Route path="/Blog" element={<Blog/>} />
         <Route path="/contact" element={<Contact/>} />
       </Routes>
