c2aa431b94d90d14798241decaab905eeae4d83c
ANTONY klerk <antomutwiri2001@gmail.com>
2023-12-12 15:44:29 +0300

    products.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 
 import Nav from './Nav'
 import Banner from './Banner'
+import Products from './Products'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <div  className="text-3xl font-bold">
     <Banner/>
     <Nav/>
+    <Products/>
     </div>
   );
 }
